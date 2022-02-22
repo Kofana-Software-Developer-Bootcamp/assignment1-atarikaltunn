146dd192111ea40620ed71795c1ed107d12bd76f
atarikaltunn <atarikaltunn@gmail.com>
2022-02-22 22:13:41 +0300

    index.html modified, script and style files added for the first time

---
+++ script.js
@@ -0,0 +1,19 @@
+var arr = [];
+
+var basvuru = {
+    isim: "",
+    email: "",
+    yas: "",
+    universite: "",
+    bolum: "",
+    sinif: "",
+    sehir: "",
+    bootcamp_katilim: "",
+    hobiler: []
+};
+
+function basvuru () {
+    var yeni_basvuru = document.getElementById("submitBtn");
+
+    console.log(yeni_basvuru);
+}
