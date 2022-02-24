aff802eebb6011a4664cb005db3d86edbe5e68b2
atarikaltunn <atarikaltunn@gmail.com>
2022-02-24 17:10:44 +0300

    pushing data into object template has created

--- script.js
+++ script.js
@@ -9,11 +9,30 @@ var basvuru = {
     sinif: "",
     sehir: "",
     bootcamp_katilim: "",
-    hobiler: []
+    hobiler: ""
 };
 
-function basvuru () {
-    var yeni_basvuru = document.getElementById("submitBtn");
+// var form = document.getElementById("bootcampForm");
 
-    console.log(yeni_basvuru);
+// document.getElementById("submitBtn").addEventListener("click", function () {
+//   alert("submitted");
+//   console.log(form);
+// });
+// console.log(1);
+function onSubmit() {
+  // console.log(2);
+  alert("submitted");
+  basvuru.isim = document.getElementById("name").value;
+  basvuru.email = document.getElementById("email").value;
+  basvuru.yas = document.getElementById("age").value;
+  basvuru.universite = document.getElementById("university").value;
+  basvuru.bolum = document.getElementById("department").value;
+  basvuru.sinif = document.getElementById("grade-choice").value;
+  basvuru.sehir = document.getElementById("city").value;
+  basvuru.bootcamp_katilim = document.getElementById("bootcamp").value;
+  basvuru.hobiler = document.getElementById("hobby").value;
+  // console.log(basvuru.isim);
+  arr.push(basvuru);
+
 }
+// console.log(3);
